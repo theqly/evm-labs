04001694e464ea0227de4033e09292eebb922ed1
theql <theqlgames@gmail.com>
2023-09-09 17:30:04 +0700

    monte carlo

--- lab1/main.c
+++ lab1/main.c
@@ -1,7 +1,24 @@
+#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
 
+double monte_carlo(int N){
+  srand(time(NULL));
+  double x, y;
+  int M = 0;
+  for(int i = 0; i < N; i++){
+    x = (double)(0 + rand() % (2 - 0 + 1));
+    y = (double)(0 + rand() % (2 - 0 + 1));
+    if(((x*x + y*y) < 1)){
+      M += 1;
+    }
+  }
+  return (double)(4*M)/N;
+}
+
 int main(int argc, char* argv[]){
-  printf("Hello World\n");
-  printf("pizda\n");
+  int N = argc;
+  double pi = monte_carlo(N);
+  printf("pi: %lf", pi);
   return 0;
 }
