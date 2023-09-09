c79779ee42281c84d6b4d79fe8901f92ebc8adf5
theql <theqlgames@gmail.com>
2023-09-10 01:30:59 +0700

    working

--- lab1/main.c
+++ lab1/main.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-double monte_carlo(int N){
-  srand(time(NULL));
+double monte_carlo(long long int N){
   double x, y;
-  int M = 0;
-  for(int i = 0; i < N; i++){
-    x = (double)(0 + rand() % (2 - 0 + 1));
-    y = (double)(0 + rand() % (2 - 0 + 1));
-    if(((x*x + y*y) < 1)){
+  long long int M = 0;
+  for(long long int i = 0; i < N; i++){
+    x = (double)rand() / RAND_MAX;
+    y = (double)rand() / RAND_MAX;
+    if(((x*x + y*y) <= 1)){
       M += 1;
     }
   }
-  return (double)(4*M)/N;
+  return (double)(4*M)/(double)N;
 }
 
 int main(int argc, char* argv[]){
-  int N = argc;
+  long long int N;
+  scanf("%lld", &N);
+  srand(time(NULL));
   double pi = monte_carlo(N);
   printf("pi: %lf", pi);
   return 0;
