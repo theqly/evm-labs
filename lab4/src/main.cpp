8a0a72e80e88112f2c58ee75bb8024d77087eb90
Гаан Артем Игоревич <a.gaan@CLASSES.NSU.RU>
2023-11-20 15:48:20 +0700

    lab4

---
+++ lab4/src/main.cpp
@@ -0,0 +1,11 @@
+#include "matrix.h"
+#include <ctime>
+#include <iostream>
+
+int main(){
+    matrix m1(512);
+    size_t start = clock();
+    matrix mi = m1.inverse(10);
+    std::cout << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << std::endl;
+    return 0;
+}
